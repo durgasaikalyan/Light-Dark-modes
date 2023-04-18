5f1fb8ef2f211e3e563aaa98e811e092360afffb
durgasaikalyan <durgasaikalyanccbp@gmail.com>
2023-04-18 17:06:57 +0000

    your message

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -1,39 +1,28 @@
 // Write your code here
 import {Component} from 'react'
+
 import './index.css'
 
 class LightDarkMode extends Component {
   state = {
-    // eslint-disable-next-line react/no-unused-state
-    isDarkMode: background.color:black;,
+    isDarkMode: true,
   }
 
-  onLightModes = () => {
+  onClickButton = () => {
     this.setState(prevState => ({isDarkMode: !prevState.isDarkMode}))
   }
 
-  getButtonText = () => {
-    const {isDarkMode} = this.state
-    // eslint-disable-next-line no-unused-vars
-    const modelClassName = isDarkMode ? 'dark-mode' : 'Light-mode'
-    // eslint-disable-next-line no-unused-vars
-    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
-  }
-
   render() {
-    // eslint-disable-next-line no-unused-vars
-    const buttonText = this.getButtonText()
+    const {isDarkMode} = this.state
+    const modeClassName = isDarkMode ? 'dark-mode' : 'light-mode'
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
 
     return (
       <div className="app-container">
-        <div className="container ${modelClassName">
-          <h1 className="heading">Click Change To Mode</h1>
-          <button
-            type="button"
-            className="button-container"
-            onClick={this.onLightModes}
-          >
-            Light Mode
+        <div className={`container ${modeClassName}`}>
+          <h1 className="heading">Click To Change Mode</h1>
+          <button type="button" onClick={this.onClickButton} className="button">
+            {buttonText}
           </button>
         </div>
       </div>
